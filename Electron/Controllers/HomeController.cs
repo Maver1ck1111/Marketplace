aff2c5224c1832080016190016a9496820d86079
Maverick <borisovd885@gmail.com>
2025-03-19 15:19:54 +0200

    Added home page

---
+++ Electron/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Electron.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
